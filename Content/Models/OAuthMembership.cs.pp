0b8e1f0140638a6e8dc86ddaa10deb858b97a24d
Chekkan <harish_babu@outlook.com>
2013-10-12 22:26:18 +0100

    added the classes for the Models folder

---
+++ Content/Models/OAuthMembership.cs.pp
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace $rootnamespace$.Models
+{
+    public class OAuthMembership
+    {
+        public string Provider { get; set; }
+        public string ProviderUserId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Content/Models/User.cs.pp
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace $rootnamespace$.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; }
+
+        public User()
+        {
+            Roles = new List<Role>();
+        }
+    }
+}
